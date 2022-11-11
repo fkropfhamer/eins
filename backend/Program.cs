ca80dd9c3a9b021985c9609476baac515958196d
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2022-11-11 14:06:58 +0100

    sharp 🗡️.

---
+++ backend/Program.cs
@@ -0,0 +1,54 @@
+using backend.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed((host) => true)
+                .AllowCredentials();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+    app.UseCors();
+}
+
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<GameHub>("/game");
+});
+
+app.UseHttpsRedirection();
+
+
+
+app.MapControllers();
+
+
+
+app.Run();
---
+++ frontend/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div v-if="connection && !isDisconnected">
+    <div v-if="hasUsername">
+      <Chat :connection="{connection}"/>
+    </div>
+    <div v-else>
+      <Username @username-set="onUsernameSet"/>
+    </div>
+  </div>
+  <div v-else-if="isDisconnected">
+    Disconnected
+  </div>
+  <div v-else>
+    Connecting...
+  </div>
+  <Game :connection="{connection}"/>
+</template>
+
+<script>
+import * as signalR from "@microsoft/signalr";
+import Username from "./Username.vue";
+import Chat from "./Chat.vue";
+import Game from "./Game.vue";
+
+export default {
+  components: {
+    Username,
+    Chat,
+    Game,
+  },
+  data() {
+    return {
+      username: "",
+      connection: null,
+      hasUsername: false,
+      isDisconnected: true,
+    };
+  },
+
+  async mounted() {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl("https://localhost:7007/game")
+      .build();
+
+    connection.on("send", (data) => {
+      console.log(data);
+    });
+
+    connection.onclose(() => {
+      this.isDisconnected = true;
+    })
+
+    await connection.start();
+    this.isDisconnected = false
+    this.connection = connection;
+    
+
+    connection.invoke("Echo", "Hello");
+  },
+
+  methods: {
+    onUsernameSet: function (username) {
+      this.username = username
+      this.connection.invoke("Join", this.username);
+      this.hasUsername = true
+    }
+  },
+};
+</script>
